f9c2f6fc6c5209788e02445cdabbdfbb3ea92852
Artem Terenin <artemterenin@gmail.com>
2023-02-06 20:54:07 +0300

    add get_user_answer

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -3,5 +3,10 @@ import prompt
 
 def welcome_user():
     print('Welcome to the Brain Games!')
-    name = prompt.string('May I have your name? ')
-    print(f'Hello, {name}!')
+    user_name = prompt.string('May I have your name? ')
+    print(f'Hello, {user_name}!')
+    return user_name
+
+def get_user_answer():
+    user_answer = prompt.string('Your answer: ')
+    return user_answer
