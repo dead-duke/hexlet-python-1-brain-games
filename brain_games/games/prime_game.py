56a4556eb2e54366a15e12dec89fce8f1b188eb6
Artem Terenin <artemterenin@gmail.com>
2023-02-15 11:31:23 +0300

    change range in prime_game.py

--- brain_games/games/prime_game.py
+++ brain_games/games/prime_game.py
@@ -7,7 +7,7 @@ prime_rule = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 def start_prime_game():
     number = randint(1, 100)
     question = f'{number}'
-    for divisor in range(1, number // 2):
+    for divisor in range(1, number // 2 + 1):
         if (number % divisor == 0 and divisor != 1 and number != divisor):
             correct_answer = 'no'
             return (question, correct_answer)
